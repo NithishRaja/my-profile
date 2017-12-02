818c9a9c794e9f1fe5fad52e2fbee5a6d5df8676
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2017-12-02 13:11:25 +0530

    refactor albumCover api route

--- server/middleware/api/albumCover.js
+++ server/middleware/api/albumCover.js
@@ -3,8 +3,16 @@ var mongo = require("mongodb");
 module.exports = function(req, res){
   // getting info from database
   var collection = mongo.DB.collection("profile");
+  var albumCover = [];
   collection.findOne({}, {}, function(err, result){
+    result.albumImages.forEach(function(image, index){
+      if(index===0){
+        albumCover = [image.images[0]];
+      }else{
+        albumCover.push(image.images[0]);
+      }
+    });
     //sending data as json
-    res.json(result.albumCover);
+    res.json(albumCover);
   });
 };
